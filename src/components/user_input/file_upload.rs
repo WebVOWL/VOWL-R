e6148dec667389d798c64eb56c3e60109fdb3a96
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-01-27 01:35:37 +0100

    fix: errors being ignored

--- src/components/user_input/file_upload.rs
+++ src/components/user_input/file_upload.rs
@@ -107,12 +107,13 @@ pub async fn handle_local(data: MultipartData) -> Result<(DataType, usize), Serv
 
         session.start_upload(&name).await?;
 
-        dtype = Path::new(&name).into();
-
         while let Ok(Some(chunk)) = field.chunk().await {
             let len = chunk.len();
             count += len;
-            session.upload_chunk(&chunk).await?;
+            session
+                .upload_chunk(&chunk)
+                .await
+                .map_err(|e| ServerFnError::new(e.to_string()))?;
             progress::add_chunk(&name, len).await;
         }
 
