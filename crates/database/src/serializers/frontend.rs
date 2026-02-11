3cfcb46cf12d0336923b0a5bff76a44049068c03
khmikkelsen <kh.mikkelsen@hotmail.com>
2026-02-11 21:24:34 +0100

    impl insert_characteristics

--- crates/database/src/serializers/frontend.rs
+++ crates/database/src/serializers/frontend.rs
@@ -1468,7 +1468,11 @@ impl GraphDisplayDataSolutionSerializer {
                         }
                     }
                     owl::INVERSE_FUNCTIONAL_PROPERTY => {
-                        self.insert_characteristic(data_buffer, triple, "InverseFunctionalProperty".to_string());
+                        self.insert_characteristic(
+                            data_buffer,
+                            triple,
+                            "InverseFunctionalProperty".to_string(),
+                        );
                     }
                     // TODO owl::INVERSE_OF => {}
                     // owl::IRREFLEXIVE_PROPERTY => {}
@@ -1560,25 +1564,27 @@ impl GraphDisplayDataSolutionSerializer {
             }
         }
     }
-    
-    fn insert_characteristic(&self, data_buffer: &mut SerializationDataBuffer, triple: Triple, arg: String) {
+
+    fn insert_characteristic(
+        &self,
+        data_buffer: &mut SerializationDataBuffer,
+        triple: Triple,
+        arg: String,
+    ) {
         let resolved = self.resolve(data_buffer, triple.id.to_string());
         match resolved {
-            Some(s) => {
-                match data_buffer.node_characteristics.get_mut(&s) {
-                    Some(char) => {
-                        char.push(arg);
-                    }
-                    None => {
-                        data_buffer.node_characteristics.insert(s, vec![arg]);
-                    }
+            Some(s) => match data_buffer.node_characteristics.get_mut(&s) {
+                Some(char) => {
+                    char.push(arg);
                 }
-            }
+                None => {
+                    data_buffer.edge_characteristics.insert(s, vec![arg]);
+                }
+            },
             None => {
                 self.add_to_unknown_buffer(data_buffer, triple.id.to_string(), triple);
             }
         }
-        
     }
 }
 
