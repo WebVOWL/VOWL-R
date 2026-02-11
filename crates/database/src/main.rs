1530cc7d8283bee4d0b87155d42a520503ce607c
khmikkelsen <kh.mikkelsen@hotmail.com>
2026-02-11 21:21:52 +0100

    log entire db for easier debugging

--- crates/database/src/main.rs
+++ crates/database/src/main.rs
@@ -1,4 +1,5 @@
 use env_logger::Env;
+use futures::StreamExt;
 use grapher::prelude::GraphDisplayData;
 use log::info;
 use rdf_fusion::{execution::results::QueryResults, store::Store};
@@ -6,7 +7,6 @@ use std::env;
 use std::path::Path;
 use vowlr_database::prelude::{GraphDisplayDataSolutionSerializer, VOWLRStore};
 use vowlr_sparql_queries::prelude::DEFAULT_QUERY;
-use futures::StreamExt;
 
 #[tokio::main]
 pub async fn main() {
@@ -24,8 +24,12 @@ pub async fn main() {
         .await
         .expect("Error inserting file");
     info!("Loaded {} quads", vowlr.session.len().await.unwrap());
-    
-    let all_stream = vowlr.session.query("SELECT * WHERE { ?s ?p ?o }").await.unwrap();
+
+    let all_stream = vowlr
+        .session
+        .query("SELECT * WHERE { ?s ?p ?o }")
+        .await
+        .unwrap();
     if let QueryResults::Solutions(mut solutions) = all_stream {
         while let Some(solution) = solutions.next().await {
             let solution = solution.unwrap();
