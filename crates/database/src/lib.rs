5a94e2f0b749ce14810a5afd4c1464d337cdfd40
khmikkelsen <kh.mikkelsen@hotmail.com>
2026-02-20 16:01:50 +0100

    Resolving comments from [[[#173](https://github.com/WebVOWL/WebVOWL-Reimagined/issues/173)](https://github.com/WebVOWL/WebVOWL-Reimagined/issues/173)](https://github.com/WebVOWL/WebVOWL-Reimagined/issues/173), write_triple now returns Result<(), SerializationError>, which we can then propagate to caller, which then adds it to the failed buffer. This should be used as last resort when writing triples, signaling that we have completely abondoned it, as it will be sent to frontend.
    + convenience macro for SerializationErrors, since it always has to box the triple, introduced to make clippy happy.
    
    Additionally, inserting characteristics works.

--- crates/database/src/lib.rs
+++ crates/database/src/lib.rs
@@ -1,4 +1,10 @@
+use std::fmt::{Display, Formatter};
+
 use grapher::prelude::{ElementType, OwlEdge, OwlType, RdfEdge, RdfType};
+use oxrdf::{BlankNodeIdParseError, IriParseError};
+use vowlr_parser::errors::VOWLRStoreError;
+
+use crate::serializers::Triple;
 
 pub mod serializers;
 pub mod store;
@@ -23,3 +29,80 @@ pub const PROPERTY_EDGE_TYPES: [ElementType; 7] = [
     ElementType::Owl(OwlType::Edge(OwlEdge::InverseOf)),
     ElementType::Rdf(RdfType::Edge(RdfEdge::RdfProperty)),
 ];
+
+pub trait SerializationErrorExt {
+    fn triple(&self) -> Option<&Triple>;
+}
+
+macro_rules! ser_err {
+    ($variant:ident($triple:expr, $msg:expr)) => {
+        $crate::SerializationErrorKind::$variant(($triple).map(Box::new), $msg)
+    };
+}
+pub(crate) use ser_err;
+
+#[derive(Debug)]
+pub enum SerializationErrorKind {
+    MissingObject(Option<Box<Triple>>, String),
+    MissingSubject(Option<Box<Triple>>, String),
+    SerializationFailed(Option<Box<Triple>>, String),
+    IriParseError(Option<Box<Triple>>, Box<IriParseError>),
+    BlankNodeParseError(Option<Box<Triple>>, Box<BlankNodeIdParseError>),
+}
+impl SerializationErrorExt for SerializationErrorKind {
+    fn triple(&self) -> Option<&Triple> {
+        match &self {
+            SerializationErrorKind::MissingObject(triple, _)
+            | SerializationErrorKind::MissingSubject(triple, _)
+            | SerializationErrorKind::SerializationFailed(triple, _)
+            | SerializationErrorKind::IriParseError(triple, _)
+            | SerializationErrorKind::BlankNodeParseError(triple, _) => {
+                triple.as_ref().map(|t| &**t)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SerializationError {
+    inner: SerializationErrorKind,
+}
+impl Display for SerializationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SerializationError: {:?}", self.inner)
+    }
+}
+
+impl SerializationErrorExt for SerializationError {
+    fn triple(&self) -> Option<&Triple> {
+        self.inner.triple()
+    }
+}
+
+impl From<SerializationErrorKind> for SerializationError {
+    fn from(error: SerializationErrorKind) -> Self {
+        SerializationError { inner: error }
+    }
+}
+
+impl From<IriParseError> for SerializationError {
+    fn from(error: IriParseError) -> Self {
+        SerializationError {
+            inner: SerializationErrorKind::IriParseError(None, Box::new(error)),
+        }
+    }
+}
+
+impl From<SerializationError> for VOWLRStoreError {
+    fn from(error: SerializationError) -> Self {
+        VOWLRStoreError::from(error.to_string())
+    }
+}
+
+impl From<BlankNodeIdParseError> for SerializationError {
+    fn from(error: BlankNodeIdParseError) -> Self {
+        SerializationError {
+            inner: SerializationErrorKind::BlankNodeParseError(None, Box::new(error)),
+        }
+    }
+}
