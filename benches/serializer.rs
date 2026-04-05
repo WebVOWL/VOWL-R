9236be16fa65bc1b0f99b09f711dc480d0170031
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-05 10:41:39 +0200

    Add benches

---
+++ benches/serializer.rs
@@ -0,0 +1,34 @@
+#![expect(clippy::expect_used, reason = "Benching is allowed to panic")]
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::path::Path;
+use vowlr_database::prelude::VOWLRStore;
+use vowlr_sparql_queries::prelude::DEFAULT_QUERY;
+
+/// Parallel serialization.
+fn par_serialize(c: &mut Criterion) {
+    let path = Path::new("crates/database/data/owl-rdf/envo.owl");
+    let store = VOWLRStore::default();
+
+    pollster::block_on(async {
+        store
+            .insert_file(path, false)
+            .await
+            .expect("Error inserting file");
+    });
+
+    let mut group = c.benchmark_group("serializer");
+    group.bench_function("Insert", |b| {
+        b.to_async(tokio::runtime::Runtime::new().expect("runtime should work"))
+            .iter(async || {
+                let _ = store
+                    .query(DEFAULT_QUERY.to_string())
+                    .await
+                    .expect("query should work");
+            });
+    });
+    group.finish();
+}
+
+criterion_group!(serializer, par_serialize);
+criterion_main!(serializer);
